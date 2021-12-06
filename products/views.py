1bec8d32cbfcf572dde1e707a9026185bbf841eb
Ellis Robinson <ellis.robinson@live.co.uk>
2021-12-06 19:39:15 +0000

    add notifications and messages for shopping bag

--- products/views.py
+++ products/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
+# Create your views here.
 
 def all_products(request):
-    ''' loads view for all products '''
+    """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    
     query = None
     categories = None
     sort = None
@@ -29,7 +30,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -38,9 +39,9 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                message.error(request, "You did't enter any search criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -52,15 +53,17 @@ def all_products(request):
         'current_categories': categories,
         'current_sorting': current_sorting,
     }
-    
+
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    ''' loads view for products detials '''
+    """ A view to show individual product details """
+
     product = get_object_or_404(Product, pk=product_id)
+
     context = {
         'product': product,
     }
-    
+
     return render(request, 'products/product_detail.html', context)
